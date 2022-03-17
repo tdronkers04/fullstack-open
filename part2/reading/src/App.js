901ee149cec08e0c5023e0b44ea5eaf4d9c7e4af
Tim Dronkers <tdronkers@gmail.com>
2022-03-17 11:59:18 -0600

    part2 first commit

---
+++ part2/reading/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+  
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => {
+          return <Note key={note.id} note={note}/>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
