81266056bba6c40b04881bef1cc062932ff1731f
Tim Dronkers <tdronkers@gmail.com>
2022-03-18 10:48:40 -0600

    refactoring reading app with db

--- part2/reading/src/App.js
+++ part2/reading/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Note from './components/Note';
+import axios from 'axios';
 
-const App = (props) => {
-  const [ notes, setNotes ] = useState(props.notes);
+const App = () => {
+  const [ notes, setNotes ] = useState([]);
   const [ newNote, setNewNote ] = useState('');
   const [ showAll, setShowAll ] = useState(true);
 
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      });
+  }
+
+  useEffect(hook, []);
+  console.log('render', notes.length, 'notes');
+  
   const addNote = (event) => {
     event.preventDefault();
     const noteObj = {
