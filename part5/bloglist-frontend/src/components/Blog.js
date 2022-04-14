dda9c224276e0e41f06eba065a0040e78cc027b8
Tim Dronkers <tdronkers@gmail.com>
2022-04-14 15:09:48 -0600

    exercise 5.7 adding state vis state for each blog component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,43 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [ visible, setVisible ] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : ''}
+  const showWhenVisible = { display: visible ? '' : 'none'}
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <p>
+          {blog.title}
+          <button onClick={toggleVisibility}>view</button>
+        </p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>
+          {blog.title}
+          <button onClick={toggleVisibility}>hide</button>
+        </p>
+        <ul>
+          <li>Author: {blog.author}</li>
+          <li>URL: {blog.url}</li>
+          <li>Likes: {blog.likes}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
