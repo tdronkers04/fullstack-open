ea75d23b1dc6387ee56f614628ad26daef7696cc
Tim Dronkers <tdronkers@gmail.com>
2022-04-25 10:54:19 -0600

    exercises 7.1 -7.3 react-router

---
+++ part7/routed-anecdotes/src/components/Notification.js
@@ -0,0 +1,36 @@
+const successStyle = {
+  color: 'green',
+  background: 'lightgrey',
+  fontSize: 20,
+  borderStyle: 'solid',
+  borderRadius: 5,
+  padding: 10,
+  marginBottom: 25,
+  maxWidth: 400,
+}
+
+const errorStyle = {
+  color: 'red',
+  background: 'lightgrey',
+  fontSize: 20,
+  borderStyle: 'solid',
+  borderRadius: 5,
+  padding: 10,
+  marginBottom: 25,
+  maxWidth: 400,
+}
+
+
+const Notification = ({ notification }) => {
+  if (notification.type === null) {
+    return null
+  }
+
+  return (
+    <div className="notify" style={notification.type === 'success' ? successStyle : errorStyle}>
+      {notification.message}
+    </div>
+  )
+}
+
+export default Notification
